f3836c24c82f9e03e3ec55f97fd2957f2c652784
Yash Mishra <mishrayash060@gmail.com>
2025-06-19 13:57:03 +0530

    Add files via upload

---
+++ client/src/assets/index.js
@@ -0,0 +1,25 @@
+import download from './download.png';
+import logo from './logo.svg';
+import preview from './preview.png';
+import aiImage from './aiImage.png'
+import backgroundImage from './backgroundImage.jpg'
+import logo1 from './logo1.png';
+import logo2 from './logo2.png';
+import logo3 from './logo3.png';
+import logo4 from './logo4.png';
+import logo5 from './logo5.png';
+import logo6 from './logo6.png';
+
+export {
+  download,
+  logo,
+  preview,
+  aiImage,
+  backgroundImage,
+  logo1,
+  logo2,
+  logo3,
+  logo4,
+  logo5,
+  logo6,
+};
