3bdf96e654b27178208a1dae7156241337d88c1f
Yash Mishra <mishrayash060@gmail.com>
2025-06-19 14:20:39 +0530

    Add files via upload

---
+++ client/src/firebaseConfig.js
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCItE3t2kPwlzGUFFA8XEvatDyVFXpdb9E",
+  authDomain: "artifai-90538.firebaseapp.com",
+  projectId: "artifai-90538",
+  storageBucket: "artifai-90538.firebasestorage.app",
+  messagingSenderId: "879208960118",
+  appId: "1:879208960118:web:efb268bbf6b2b9ec9a0f57"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);  // Export Firestore instance
+const googleProvider = new GoogleAuthProvider();
+
+export { auth, db, googleProvider };
